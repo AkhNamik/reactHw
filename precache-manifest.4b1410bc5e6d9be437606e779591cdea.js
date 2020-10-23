a34627f98252ca49edff2ee1c6581175dbf5dfcd
Namik <ahmedov.namik@gmail.com>
2020-10-23 21:31:25 +0300

    Updates

---
+++ precache-manifest.4b1410bc5e6d9be437606e779591cdea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b674f423a165a57355258f23b376797a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7189e42872f3a1877636",
+    "url": "./static/css/main.a6f798b8.chunk.css"
+  },
+  {
+    "revision": "091f8de2fd523fc2d3fd",
+    "url": "./static/js/2.e5896b88.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "./static/js/2.e5896b88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7189e42872f3a1877636",
+    "url": "./static/js/main.98979680.chunk.js"
+  },
+  {
+    "revision": "31b266c283f5a6c6da5c",
+    "url": "./static/js/runtime-main.231e19e2.js"
+  },
+  {
+    "revision": "e45d9d1f63b5b15b9ebf933e7d9088d3",
+    "url": "./static/media/252.e45d9d1f.png"
+  }
+]);
